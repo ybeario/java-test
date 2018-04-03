ef1d7949a04d948fb5a9f95f6b962d7e69610569
ybeario <571523412@qq.com>
2018-04-03 10:40:31 +0800

    网易，招行

---
+++ src/main/java/com/bear/newcoder/MainShudui.java
@@ -0,0 +1,26 @@
+package com.bear.newcoder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainShudui {
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] lines=br.readLine().split(" ");
+		int n=Integer.parseInt(lines[0]);
+		int k=Integer.parseInt(lines[1]);
+		int x = 1;
+		int y=1;
+		int num=0;
+		for (;x<=n; x++) {
+			for (y=1; y <= n; y++) {
+				if (x%y>=k) {
+					num++;
+				}
+			}
+			
+		}
+		System.out.println(num);
+	}
+}
---
+++ src/main/java/com/bear/newcoder/Solution.java
@@ -0,0 +1,27 @@
+package com.bear.newcoder;
+
+
+ class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+	public int run(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int left=run(root.left);
+        int right=run(root.right);
+        if (left==0 || right==0) {
+			return left+right+1;
+		}
+        return Math.min(left, right)+1;
+    }
+	
+}
