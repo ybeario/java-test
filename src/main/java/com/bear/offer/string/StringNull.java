3fde6d5c461184584545f68d7d40aa693a179fdf
ybeario <571523412@qq.com>
2018-09-18 10:39:52 +0800

    Signed-off-by: ybeario <571523412@qq.com>

---
+++ src/main/java/com/bear/offer/string/StringNull.java
@@ -0,0 +1,17 @@
+package com.bear.offer.string;
+
+/**
+ * @author Y.bear
+ * @version 创建时间：2018年9月10日 下午4:22:24 类说明
+ */
+public class StringNull {
+
+	static String s1;
+	static String s2;
+
+	public static void main(String[] args) {
+
+		System.out.println(s1 + s2);
+
+	}
+}
